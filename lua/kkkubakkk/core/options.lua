1ac5307b4c84a011c95ed67920fbcd262cbffe53
KKKUBAKKK <kuba.kindracki@gmail.com>
2025-08-23 23:37:12 +0200

    Add copilot chat and completion + fix some keybindings

--- lua/kkkubakkk/core/options.lua
+++ lua/kkkubakkk/core/options.lua
@@ -1,15 +1,15 @@
 vim.cmd("let g:netrw_liststyle = 3")
 
-local opt = vim.opt -- for conciseness
+local opt = vim.opt
 
 -- line numbers
 opt.relativenumber = true -- show relative line numbers
-opt.number = true -- shows absolute line number on cursor line (when relative number is on)
+opt.number = true         -- shows absolute line number on cursor line (when relative number is on)
 
 -- tabs & indentation
-opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
-opt.shiftwidth = 2 -- 2 spaces for indent width
-opt.expandtab = true -- expand tab to spaces
+opt.tabstop = 4       -- 4 spaces for tabs (prettier default is 2)
+opt.shiftwidth = 4    -- 4 spaces for indent width
+opt.expandtab = true  -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 -- line wrapping
@@ -17,7 +17,7 @@ opt.wrap = false -- disable line wrapping
 
 -- search settings
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+opt.smartcase = true  -- if you include mixed case in your search, assumes you want case-sensitive
 
 -- cursor line
 opt.cursorline = true -- highlight the current cursor line
@@ -25,10 +25,9 @@ opt.cursorline = true -- highlight the current cursor line
 -- appearance
 
 -- turn on termguicolors for nightfly colorscheme to work
--- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
 opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+opt.signcolumn = "yes"  -- show sign column so that text doesn't shift
 
 -- backspace
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
@@ -40,5 +39,5 @@ opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
 
--- turn off swapfile
-opt.swapfile = false
+-- swapfile
+opt.swapfile = true
--- lua/kkkubakkk/plugins/copilot.lua
+++ lua/kkkubakkk/plugins/copilot.lua
@@ -1,11 +1,35 @@
 return {
-	"zbirenbaum/copilot.lua",
-	cmd = "Copilot",
-	build = ":Copilot auth",
-	config = function()
-		require("copilot").setup({
-			suggestion = { enabled = false },
-			panel = { enabled = false },
-		})
-	end,
+    "github/copilot.vim",
+    event = "InsertEnter", -- Load Copilot when entering insert mode
+    config = function()
+        vim.g.copilot_no_tab_map = false
+        vim.api.nvim_set_keymap("i", "<S-Tab>", 'copilot#Accept("<CR>")', { expr = true, silent = true })
+        vim.g.copilot_filetypes = {
+            ["TelescopePrompt"] = false,
+            ["markdown"] = true,
+            [""] = false,
+        }
+    end,
+    keys = {
+        {
+            "<leader>ce",
+            function()
+                vim.cmd("Copilot enable")
+                vim.notify("Copilot enabled", vim.log.levels.INFO)
+            end,
+            desc = "Enable Copilot"
+        },
+        {
+            "<leader>cd",
+            function()
+                vim.cmd("Copilot disable")
+                vim.notify("Copilot disabled", vim.log.levels.INFO)
+            end,
+            desc = "Disable Copilot"
+        },
+        { "<leader>cp", "<cmd>Copilot panel<cr>", desc = "Open Copilot Panel" },
+        { "<leader>cf", "<cmd>Copilot fix<cr>",   desc = "Fix Copilot Suggestion" },
+        { "<leader>cg", "<cmd>Copilot goto<cr>",  desc = "Go to Copilot Suggestion" },
+        { "<leader>cr", "<cmd>Copilot reset<cr>", desc = "Reset Copilot Session" },
+    },
 }
