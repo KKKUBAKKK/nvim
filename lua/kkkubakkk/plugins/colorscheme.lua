fa0c2f2dd8e81753181db010bd941ea43cb638d7
Kindracki, Jakub Tomasz <jakub.tomasz.kindracki@intel.com>
2026-06-02 19:54:20 +0200

    Change theme

--- lua/kkkubakkk/plugins/colorscheme.lua
+++ lua/kkkubakkk/plugins/colorscheme.lua
@@ -1,40 +1,24 @@
+-- Gruvbox: warm, retro, low-blue colorscheme.
+-- Tweak `contrast` to "hard" for a darker background or "soft" for a lighter
+-- one. `background` is set to dark in core/options.lua; flip it to "light" there
+-- (or with `:set background=light`) to use Gruvbox's light variant.
 return {
   {
-    "folke/tokyonight.nvim",
-    priority = 1000, -- make sure to load this before all the other start plugins
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000, -- load before other start plugins
     config = function()
-      local bg = "#011628"
-      local bg_dark = "#011423"
-      local bg_highlight = "#143652"
-      local bg_search = "#0A64AC"
-      local bg_visual = "#275378"
-      local fg = "#CBE0F0"
-      local fg_dark = "#B4D0E9"
-      local fg_gutter = "#627E97"
-      local border = "#547998"
-
-      require("tokyonight").setup({
-        style = "night",
-        on_colors = function(colors)
-          colors.bg = bg
-          colors.bg_dark = bg_dark
-          colors.bg_float = bg_dark
-          colors.bg_highlight = bg_highlight
-          colors.bg_popup = bg_dark
-          colors.bg_search = bg_search
-          colors.bg_sidebar = bg_dark
-          colors.bg_statusline = bg_dark
-          colors.bg_visual = bg_visual
-          colors.border = border
-          colors.fg = fg
-          colors.fg_dark = fg_dark
-          colors.fg_float = fg
-          colors.fg_gutter = fg_gutter
-          colors.fg_sidebar = fg_dark
-        end,
+      require("gruvbox").setup({
+        terminal_colors = true,
+        contrast = "", -- "hard" | "soft" | "" (medium, default)
+        bold = true,
+        italic = {
+          strings = false,
+          comments = true,
+        },
+        transparent_mode = false,
       })
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
+      vim.o.background = "dark"
+      vim.cmd([[colorscheme gruvbox]])
     end,
   },
 }
